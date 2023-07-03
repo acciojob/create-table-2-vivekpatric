417323bf0bf28fc20709c4995651f655f80cf79f
Vivek Patric R <96299357+vivekpatric@users.noreply.github.com>
2023-07-03 20:29:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 function createTable() {
     //Write your code here
+	var rn = prompt("Input number of rows");; // Number of rows (can be dynamically determined as per your requirement)
+      var cn = prompt("Input number of columns"); // Prompt for number of columns
+
+      if(cn) {
+        // Check if the user entered a value and clicked OK
+        var table = document.getElementById("myTable");
+        table.innerHTML = ""; // Clear the existing table content
+
+        // Create the table rows and cells
+        for (var i = 0; i < rn; i++) {
+          var row = table.insertRow(i);
+          for (var j = 0; j < cn; j++) {
+            var cell = row.insertCell(j);
+            cell.innerHTML = "Row-" + (i ) + " Column-" + (j );
+          }
+        }
+      }
+    }
   
-}
